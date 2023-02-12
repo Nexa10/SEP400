f7981482fbee3f4c9d4c1e4030ce46891fe80272
Dennis Audu <aududennis2016@gmail.com>
2023-02-12 16:27:22 -0500

    lab3 che

--- Labs/lab1_pseudo.cpp
+++ Labs/lab1_pseudo.cpp
@@ -4,40 +4,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-
 using namespace std;
 char name[100];
 
-void change(char* str){
-    char hey [64] = "is not the goat";
-    char temp[64] ;
-    if(strlen(name) > 0){
-        strncpy(temp, name, sizeof(temp));
+void change(char *str)
+{
+    char hey[64] = "is not the goat";
+    char *temp;
+    strncpy(temp, str, sizeof(temp));
+    // if (strlen(name) > 0)
+    // {
 
-        
-        memset(name,'\0', sizeof(name));
+    // }
+
+    if (strncmp(str, "name", 4) == 0)
+    {
+        memset(name, '\0', sizeof(name));
+        strncpy(name, "Ronaldo ", sizeof(name));
+        strcat(name, hey);
     }
-    
-   if (strcmp(temp, "name") == 0){
-            strncpy(name, "Ronaldo ", sizeof(name));
-            strcat(name, hey);
-   }
-   else if((strcmp(temp, "sleep")) == 0){
-       sleep(2);
-        //printf("Slept");
-        cout << "slept" << endl;
-   }
-else{
-            cout << "None" << endl;
+    else if (strncmp(str, "sleep", 5) == 0)
+    {
+        sleep(2);
+        printf("Slept");
+        // cout << "slept" << endl;
     }
-    
+    else
+    {
+        cout << "None" << endl;
+    }
 }
 
 int main()
 {
     strncpy(name, "name", 100);
+    cout << "old Name: " << name << endl;
     change(name);
-    cout << "New Name: " << name << endl;
+    printf("New name: %s\n", name);
 
     return 0;
 }
