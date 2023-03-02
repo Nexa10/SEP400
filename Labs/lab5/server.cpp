be320ef62e0946d4cb514ae9d404a93d5bebc7ef
nexa <nexa@nexaOS.myguest.virtualbox.org>
2023-03-01 22:34:40 -0500

    implement

--- Labs/lab5/server.cpp
+++ Labs/lab5/server.cpp
@@ -1,10 +1,13 @@
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <strings.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/un.h>
+#include <netinet/in.h>
 #include <unistd.h>
-#include <pthread.h>
+#include <errno.h>
+#include <signal.h>
 #include <iostream>
 #include <string>
 #include <queue>
@@ -14,9 +17,13 @@ using namespace std;
 const char IP_ADDR[] = "127.0.0.1"; 
 bool is_running = true;
 queue<string> message;
+queue<int*>fds;
+pthread_t client_threads [MAX_CLIENTS];
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t thr_mutex = PTHREAD_MUTEX_INITIALIZER;
 void sigHandler(int sig);
 void* readThread(void* arg);
+void* threadFunc(void *arg);
 void errorMsg(const char* msg);
 void printQueue(queue<string> str);
 
@@ -28,14 +35,22 @@ int main(int argc, char* argv[]){
     sa.sa_handler = &sigHandler;
     sa.sa_flags = SA_RESTART;
 
+    for(int i=0; i<MAX_CLIENTS; i++){
+        pthread_create(&client_threads[i], NULL, threadFunc, NULL);
+    }
+
     fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
     if(fd < 0){
         errorMsg("Error: Socket failed");
     }
 
     bzero((char *)&addr, sizeof(addr));
+//    addr.sin_family = AF_INET;
+//    addr.sin_addr.s_addr = inet_addr(IP_ADDR);
+//    addr.sin_port = htons(atoi(argv[1]));
+
     addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = inet_addr(IP_ADDR);
+    inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
     addr.sin_port = htons(atoi(argv[1]));
     // if((ret = inet_pton(AF_INET, IP_ADDR, addr.sin_addr)) == 0){
     //     close(fd);
@@ -48,34 +63,40 @@ int main(int argc, char* argv[]){
     }
 
     //listen
-    if(listen(fd, 5) < 0){
+    if(listen(fd, 3) < 0){
         close(fd);
-        errorMsg("Error: listen failed")
+        errorMsg("Error: listen failed");
     }
+    cout << "Waiting..." << endl;
 
     int num_client = 0;
-    pthread_t clientRecv;
-    fd_set readfds;  
+    int clilen = sizeof(destaddr);
+
     while(is_running){
         //accept
-        if(num_client < MAX_CLIENTS){
-            if(cl = accept(fd, (struct sockaddr *)&destaddr, sizeof(destaddr)) < 0){
 
-            }
-            else{
-                num_client += 1;
-            }
+        if((cl = accept(fd, (struct sockaddr *)&destaddr, (socklen_t*)&clilen)) < 0){
+            errorMsg("Error: Accept");
         }
+        int *pClient = new int(sizeof(int));
+        *pClient = cl;
+        pthread_mutex_lock(&thr_mutex);
+        fds.push(pClient);
+        pthread_mutex_unlock(&thr_mutex);
 
         pthread_mutex_lock(&mutex);
-        print(message);
+        printQueue(message);
         if(!message.empty()) while(!message.empty()) message.pop();
         pthread_mutex_unlock(&mutex);
         sleep(1);
     }
     //send quit
-    
-    pthread_join(&clientRecv, NULL); 
+    send(cl, "Quit", 4, 0);
+    for(int i=0; i<MAX_CLIENTS; i++) {
+        pthread_join( client_threads[i], NULL);
+    }
+    close(cl);
+    close(fd);
     return 0;
 }
 
@@ -84,10 +105,32 @@ void sigHandler(int sig){
         is_running = false;
     }
 }
+
+void* threadFunc(void *arg){
+    while(is_running){
+        pthread_mutex_lock(&thr_mutex);
+        int *client = fds.front();
+        if(!fds.empty()) fds.pop();
+        pthread_mutex_lock(&thr_mutex);
+        if(client != NULL)
+            readThread(client);
+    }
+    pthread_exit(NULL);
+}
+
 void* readThread(void* arg){
     int fd = *(int *)arg;
     char buf[4026];
-    int rd = read(fd, buf, sizeof(buf)-1); 
+    struct timeval tv;
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+
+    setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
+    bzero(buf, sizeof(buf));
+    int rd = read(fd, buf, sizeof(buf)-1);
+    if(rd < 0){
+        errorMsg("Error: Read failed");
+    }
     while(is_running){
         pthread_mutex_lock(&mutex);  
         message.push(buf);
